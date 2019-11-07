af1a172875840df8b9783afa8978288aa474b492
Ubuntu <guillem.ballonga@gmail.com>
2019-11-07 16:56:45 +0000

    micropostXissue

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -28,7 +28,7 @@ class MicropostsController < ApplicationController
 
     respond_to do |format|
       if @micropost.save
-        format.html { redirect_to @micropost, notice: 'Micropost was successfully created.' }
+        format.html { redirect_to @micropost, notice: 'Issue was successfully created.' }
         format.json { render :show, status: :created, location: @micropost }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class MicropostsController < ApplicationController
   def update
     respond_to do |format|
       if @micropost.update(micropost_params)
-        format.html { redirect_to @micropost, notice: 'Micropost was successfully updated.' }
+        format.html { redirect_to @micropost, notice: 'Issue was successfully updated.' }
         format.json { render :show, status: :ok, location: @micropost }
       else
         format.html { render :edit }
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,5 +5,5 @@ class Micropost < ApplicationRecord
   validates :content, length: { maximum: 140 },
                       presence: true
  validates :title, presence: true                    
- #hola
+ 
 end
