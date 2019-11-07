041a8b41adb58cc329874e983c4e7989539aa906
Ubuntu <guillem.ballonga@gmail.com>
2019-11-07 14:36:42 +0000

    Halloween release

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_103431) do
+ActiveRecord::Schema.define(version: 2019_11_07_142552) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
