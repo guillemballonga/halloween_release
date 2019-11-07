8adb00d53135661d67afe4775d862d3abafd1889
Ubuntu <guillem.ballonga@gmail.com>
2019-11-07 16:20:27 +0000

    Halloween release

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,5 +4,6 @@ class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, length: { maximum: 140 },
                       presence: true
-  validates :title, presence: true                    
+ validates :title, presence: true                    
+ #hola
 end
